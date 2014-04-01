8ae97a9ac2458cc84fd1c323eaf1b68289aa0866
Draegert <alexander_draegert@hotmail.com>
2014-04-01 14:30:38 +0200

    Sould now work with byte[] but not tested.

--- src/dk/aau/cs/giraf/pictogram/AudioPlayer.java
+++ src/dk/aau/cs/giraf/pictogram/AudioPlayer.java
@@ -19,12 +19,16 @@
 
 package dk.aau.cs.giraf.pictogram;
 
+import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.util.Log;
+import android.media.AudioTrack;
 
 import java.io.IOException;
+
+
 
 /**
  * @author Croc
@@ -32,31 +36,23 @@ import java.io.IOException;
  */
 public enum AudioPlayer{
     INSTANCE;
-    private static MediaPlayer mediaPlayer;
+    private static AudioTrack audioTrack;
     private final static String TAG = "AudioPlayer";
+    private boolean stillPlaying = true;
+    private int soundFrequency = 44100;
+    private Thread soundThread;
 
     /**
      * Opens the {@link MediaPlayer} this is called as soon as the object
      * appears in memory.
      */
     private AudioPlayer(){
-        open();
     }
 
-    /**
-     * Creates the {@link MediaPlayer} object, this object is used for playing
-     * audio.
-     */
-    public static void open(){
-        mediaPlayer = new MediaPlayer();
-    }
 
-    /**
-     * Releases the {@link MediaPlayer}, making it a null object.
-     */
-    public static void close(){
-        mediaPlayer.release();
-    }
+
+
+
     
     /**
      * This method tries to reset the {@link MediaPlayer}. If it did not
@@ -64,13 +60,12 @@ public enum AudioPlayer{
      */
     public static void reset(){
         try{
-            Log.i(TAG,"Resetting AudioPlayer.");
-            mediaPlayer.reset();
+            Log.i(TAG, "Resetting AudioPlayer.");
+
+
         } catch (IllegalStateException e){
             Log.e(TAG,"Could not reset AudioPlayer.");
             Log.i(TAG, "Attempting to reopen AudioPlayer.");
-            close();
-            open();
         }
     }
 
@@ -88,32 +83,62 @@ public enum AudioPlayer{
      * @param path the path to a piece of audio.
      * @param listener the callback that will be run
      */
-    public void play(final byte[] sound_data, final OnCompletionListener listener){//Blob type should be changed to whatever OasisLib returns as sound.
+    public void play(final byte[] sound_data, final OnCompletionListener listener){
         //TODO find out if we should stop any ongoing playback or not, current implementation stops playback. Making it an user defined option might be too much?
         //TODO play is blocking, make this not true by implementing a seperate thread?
         try {
+            //http://audioprograming.wordpress.com/2012/10/18/a-simple-synth-in-android-step-by-step-guide-using-the-java-sdk/
+            soundThread = new Thread()
+            {
+                public void run()
+                {
+                    //Set priority, should maybe not be Max, but not sure.
+                    setPriority(Thread.MAX_PRIORITY);
 
-            mediaPlayer.reset();
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            mediaPlayer.setDataSource("");//URI vs URL (Special Characters? How are the stored in LocalDB?)
-            mediaPlayer.prepare();
+                    int minimumBufferSize = AudioTrack.getMinBufferSize(soundFrequency, AudioFormat.CHANNEL_CONFIGURATION_STEREO, AudioFormat.ENCODING_PCM_16BIT);
 
-            if (listener != null){
-                mediaPlayer.setOnCompletionListener(listener);
-            }
+                    AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, soundFrequency, AudioFormat.CHANNEL_CONFIGURATION_STEREO, AudioFormat.ENCODING_PCM_16BIT, minimumBufferSize, AudioTrack.MODE_STREAM);
 
-            mediaPlayer.start();
+                    int amp = 10000;
+                    double twoPi = 8.* Math.tan(1.);
+                    double fr = 440.f;
+                    double ph = 0.0;
+
+                    audioTrack.play();
+
+                    while (stillPlaying == true)
+                    {
+                        for (int i=0; i < minimumBufferSize; i++)
+                        {
+                            sound_data[i] = (byte) (amp * Math.sin(ph));
+                            ph += twoPi * fr / soundFrequency;
+                        }
+                        audioTrack.write(sound_data, 0, minimumBufferSize);
+                    }
+                    audioTrack.stop();
+                    audioTrack.release();
+                }
+            };
+            soundThread.start();
+
+            soundThread.destroy();
+
         } catch (IllegalArgumentException e) {
             Log.e(TAG, "Illegal argument exception, please check that all arguments were correctly formatted.");
             e.printStackTrace();
         } catch (IllegalStateException e) {
-            Log.e(TAG, "AudioPlayer was in the wrong state, try reset.");
+            Log.e(TAG, "AudioPlayer was in the   wrong state, try reset.");
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (Exception e) {
             //TODO make this exception take proper action instead of just failing and shutting up.
             //TODO make this exception more informative.
-            Log.e(TAG, "play throws an IOException.");
+            Log.e(TAG, "play throws an Exception.");
             e.printStackTrace();
         }
+
     }
+
+
+
+
 }
