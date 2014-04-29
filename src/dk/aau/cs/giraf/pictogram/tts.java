a827f787b301db76f3dd982bacbf8dbaffc3d4b1
Christian OKeeffe <christian@joedal.dk>
2014-04-29 15:31:05 +0200

    Try to tts

---
+++ src/dk/aau/cs/giraf/pictogram/tts.java
@@ -0,0 +1,50 @@
+package dk.aau.cs.giraf.pictogram;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Environment;
+
+import org.apache.http.util.ByteArrayBuffer;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Christian on 28-04-14.
+ */
+public class tts {
+    public static void PlayText(String textToPlay)
+    {
+
+        String url="http://www.translate.google.com/translate_tts?ie=UTF-8&q="+textToPlay+"&tl=da_dk";
+        DownloadFile(url,"test");
+    }
+
+    public static void DownloadFile(String imageURL, String fileName) {
+        try{
+        URL url = new URL(imageURL);
+        File file = new File(fileName);
+
+        long startTime = System.currentTimeMillis();
+        URLConnection ucon = url.openConnection();
+        InputStream is = ucon.getInputStream();
+        BufferedInputStream bis = new BufferedInputStream(is);
+        ByteArrayBuffer baf = new ByteArrayBuffer(50);
+        int current = 0;
+        while ((current = bis.read()) != -1)
+            baf.append((byte) current);
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(baf.toByteArray());
+        fos.close();
+        }
+        catch (IOException e){
+                e.printStackTrace();
+        }
+    }
+}
